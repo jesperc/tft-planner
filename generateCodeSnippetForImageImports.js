2e9bf48342c634382d6e90c0c507fe74f678ead5
Jesper Corell <jesper.corell@gmail.com>
2020-03-19 14:02:59 +0100

    :sparkles: generate code snippet for image imports

---
+++ generateCodeSnippetForImageImports.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const filenames = fs.readdirSync('src/resources/images/')
+
+let importString = ''
+let exportString = 'export {\n'
+
+filenames.forEach(filename => {
+  const name = filename
+    .split('.')
+    .slice(0, -1)
+    .join('.')
+  const key = name.charAt(0).toLowerCase() + name.slice(1)
+  importString += `import ${key} from '../resources/images/${filename}';\n`
+  exportString += `${key},\n`
+})
+
+fs.appendFile('codeSnippet.js', `${importString}${exportString}}`, error => console.log(error))
